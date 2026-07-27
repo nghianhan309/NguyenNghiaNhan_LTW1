b74fce46474422bcc53f399820365000afb7fecf
Nguyen Nghia Nhan <nghianhan30092k5@gmail.com>
2026-07-27 14:56:13 +0700

    Init commit

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lap Trinh Web 1</title>
+    <title>Document</title>
 </head>
 <body>
-    <h1><?php echo "Xin chao, toi la Nguyen Nghia Nhan"; ?></h1>
+    <h1>Xin chào AI</h1>
 </body>
 </html>

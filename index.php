829e65faae33d7238a8a4026df1041478d6b7dea
Nguyen Nghia Nhan <nghianhan30092k5@gmail.com>
2026-07-27 14:47:52 +0700

    Cập nhật khung HTML cho index.php

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
-<?php
-    echo "Xin chao, toi la Nguyen Nghia Nhan";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lap Trinh Web 1</title>
+</head>
+<body>
+    <h1><?php echo "Xin chao, toi la Nguyen Nghia Nhan"; ?></h1>
+</body>
+</html>
